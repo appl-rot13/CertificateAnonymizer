1c438446d8da2ec2220e262383d17ed836236ef4
rot <appl.rot13@gmail.com>
2024-04-26 16:04:00 +0900

    Create new

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿
+namespace CertificateAnonymizer
+{
+    using System.IO;
+    using System.Windows;
+
+    using SkiaSharp;
+
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnPreviewDragOver(DragEventArgs e)
+        {
+            base.OnPreviewDragOver(e);
+
+            e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None;
+            e.Handled = true;
+        }
+
+        protected override void OnPreviewDrop(DragEventArgs e)
+        {
+            base.OnPreviewDrop(e);
+
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                return;
+            }
+
+            var filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (filePaths == null)
+            {
+                return;
+            }
+
+            foreach (var filePath in filePaths)
+            {
+                try
+                {
+                    Anonymize(filePath);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show($"{exception}");
+                }
+            }
+        }
+
+        private static void Anonymize(string readFilePath)
+        {
+            if (string.IsNullOrWhiteSpace(readFilePath) || Path.GetExtension(readFilePath) != ".jpg")
+            {
+                return;
+            }
+
+            var writeFilePath = Path.Combine(
+                Path.GetDirectoryName(readFilePath) ?? string.Empty,
+                string.Format(
+                    "{0}_Anonymized{1}",
+                    Path.GetFileNameWithoutExtension(readFilePath),
+                    Path.GetExtension(readFilePath)));
+
+            using (var bitmap = SKBitmap.Decode(readFilePath))
+            using (var canvas = new SKCanvas(bitmap))
+            {
+                canvas.DrawRect(100, 720, 700, 80, new SKPaint { Color = SKColors.White });
+                canvas.DrawBitmap(bitmap, 0, 0);
+
+                using (var stream = new SKFileWStream(writeFilePath))
+                {
+                    bitmap.Encode(stream, SKEncodedImageFormat.Jpeg, 100);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 # Certificate Anonymizer
 
+A tool to anonymize Runtrip's certificate files.
+
+## Usage
+
+Drag and drop certificate files onto the window.
+Anonymized files with "_Anonymized" suffix appended to their filenames will be created.
+
 ## License
 
 This software is licensed under the [Unlicense](LICENSE).
